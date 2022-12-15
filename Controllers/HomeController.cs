4c05c81b31e5cd5a08a7660d0c351577f792791c
felix <felixdeswaef@gmail.com>
2022-12-15 09:57:39 +0100

    added controller logic

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,21 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DotNet;
+using DotNet.Models;
 
 
 namespace DotNet.Controllers
 {
     public class HomeController : Controller
     {
-        
+
+        public ActionResult e404()
+        {
+            return View();
+        }
         public ActionResult Index()
         {
-            
-            return View();
+            if (Request.HttpMethod == "GET")
+            {
+                return View();
+            }
+            else if (Request.HttpMethod == "POST")
+            {
+                Session["UID"] = Request.Form["klantid"];
+            }
+            return Redirect("/Home/e404");
+
         }
         public ActionResult Reserv()
         {
-            return View();
+            data data = new data();
+            if (Request.HttpMethod == "GET")
+            {
+                
+                Session["data"] = data;
+                return View();
+            }
+            else if (Request.HttpMethod == "POST")
+            {
+                data.Reserveer((int)Session["UID"], (int) Convert.ToInt32(Request.Form["SLOC"]), Convert.ToInt32(Request.Form["ELOC"]), Convert.ToInt32(Request.Form["VEHI"]), Request.Form["STAR"], Convert.ToInt32(Request.Form["DLEN"]));
+                return Redirect("/Home/Overzicht");
+            }
+            return Redirect("/Home/e404");
+            
+
         }
 
         public ActionResult Regis()
--- Models/data.cs
+++ Models/data.cs
@@ -14,10 +14,20 @@ namespace DotNet.Models
         public data()
         {
             this.locs = new ArrayList();
+            locs.Add(new locatie(1, "berlin"));
+            locs.Add(new locatie(2, "brussels"));
+            locs.Add(new locatie(3, "paris"));
             this.wagens = new ArrayList();
+            wagens.Add(new wagen(1, "golf", 23.5));
+            wagens.Add(new wagen(2, "tiguan", 22.5));
+            wagens.Add(new wagen(3, "sirocco", 21.5));
+        }
+        public void Reserveer(int uid,int sloc,int eloc,int vehi, string date, int days)
+        {
+
         }
 
         public ArrayList Locs { get => locs; set => locs = value; }
-        public ArrayList Wagens { get => wagens; set => wagens = value; }
+        public ArrayList Wagens {get => wagens; set => wagens = value; }
     }
 }
